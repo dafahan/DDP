bf0bc5f92e08b7f668f4414412fbd20ef4a5fc39
Dafahan <dafahanid@gmail.com>
2022-12-08 08:59:21 +0700

    r

---
+++ RAW LABYRINTH.cpp
@@ -0,0 +1,17 @@
+Compiling single file...
+--------
+- Filename: C:\Users\Praktikan\Documents\erija\ddd.cpp
+- Compiler Name: MinGW GCC 6.3.0
+
+Processing C++ source file...
+--------
+- C++ Compiler: C:\MinGW\bin\g++.exe
+- Command: g++.exe "C:\Users\Praktikan\Documents\erija\ddd.cpp" -o "C:\Users\Praktikan\Documents\erija\ddd.exe" -lncurses -I"C:\MinGW\include" -I"C:\MinGW\mingw32\include" -I"C:\MinGW\lib\gcc\mingw32\6.3.0\include" -I"C:\MinGW\lib\gcc\mingw32\6.3.0\include\c++" -L"C:\MinGW\lib" -L"C:\MinGW\mingw32\lib" -static-libstdc++ -static-libgcc
+
+Compilation results...
+--------
+- Errors: 0
+- Warnings: 0
+- Output Filename: C:\Users\Praktikan\Documents\erija\ddd.exe
+- Output Size: 2,03997993469238 MiB
+- Compilation Time: 0,50s
